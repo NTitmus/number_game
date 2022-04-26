4115cb873c36b70f7b844aeab74c3e68945af1c3
Nicola Titmus <nicola.koo@cantab.net>
2022-04-26 16:44:19 +0100

    2 players and win functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ButtonBar from './ButtonBar';
 
 const App = () => {
 
     //State - total number
-    const [total, setTotal] = useState(0)
+    const [total, setTotal] = useState(0);
+    const [win, setWin] = useState(false);
+    const [player1Turn, setPlayer1Turn] = useState(true)
+
+    //Target number
+    const target = 10;
 
     //Array of possible buttons to pass into ButtonBar
     const numbers = [
@@ -22,22 +27,38 @@ const App = () => {
     const addToTotal = (n) => {
         console.log('from app', n.target.value)
         setTotal(total+Number(n.target.value))
+        
     }
 
+    //Check if person has won
+    useEffect(()=>{
+        if(total===target){
+            setWin(true);
+        }
+        if(total===0||total===target){return}
+        setPlayer1Turn(!player1Turn);
+        
+    },[total])
 
+    const showPlayer = player1Turn ? <h3 style={{color:'orange'}}>Player 1</h3> : <h3 style={{color:'blue'}}>Player 2</h3>
+    const showWin = win ? <div style={{color:'red'}}>{showPlayer} WINS!</div> : null;
+
+    
 
     return (
         <div>
             <h2>Number Game</h2>
+            <p>{`The target is ${target}`}</p>
+            {showPlayer}
             <ButtonBar numberList={numbers} 
             total={total} 
+            target={target}
             onButtonPress={(num)=>addToTotal(num)}/>
             <h1>{total}</h1>
+            {showWin}
         </div>
     );
 
 }
-
-
 
 export default App
